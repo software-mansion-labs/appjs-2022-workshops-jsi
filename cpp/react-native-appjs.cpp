dedb511f38e76708207097185682ebdf630ad302
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-05-27 15:50:09 +0200

    chore: initial commit

---
+++ cpp/react-native-appjs.cpp
@@ -0,0 +1,7 @@
+#include "react-native-appjs.h"
+
+namespace example {
+	int multiply(float a, float b) {
+		return a * b;
+	}
+}
---
+++ ios/Appjs.h
@@ -0,0 +1,11 @@
+#import <React/RCTBridgeModule.h>
+
+#ifdef __cplusplus
+
+#import "react-native-appjs.h"
+
+#endif
+
+@interface Appjs : NSObject <RCTBridgeModule>
+
+@end
